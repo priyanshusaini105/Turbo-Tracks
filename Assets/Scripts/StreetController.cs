62f1790ca8e65102b2d92468c788a5e185a8e12c
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-18 03:03:29 +0530

    Destroy Previous RoadPlane

--- Assets/Scripts/StreetController.cs
+++ Assets/Scripts/StreetController.cs
@@ -6,33 +6,33 @@ public class StreetController : MonoBehaviour
 {
     public float roadPlaneLength = 100f;
     public GameObject roadPlanePrefab;
+    public GameObject currentRoadPlane;
+    public GameObject nextRoadPlane;
 
     private GameObject player;
-    private bool isRoadPlaneSpawned = false;
+    // private bool isRoadPlaneSpawned = false;
 
     private void Start()
     {
         player = GameObject.Find("Player");
+        // Instantiate the initial road plane
+        currentRoadPlane = Instantiate(roadPlanePrefab, Vector3.zero, Quaternion.identity);
+        nextRoadPlane = Instantiate(roadPlanePrefab, new Vector3(0, 0, roadPlaneLength), Quaternion.identity);
     }
 
     private void Update()
     {
-        if (!isRoadPlaneSpawned)
-        {
-            SpawnNextRoadPlane();
-        }
+        SpawnNextRoadPlane();
     }
 
     private void SpawnNextRoadPlane()
     {
-        float playerZ = player.transform.position.z;
-        float roadPlaneZ = transform.position.z;
-        
-        if (playerZ > roadPlaneZ)
+        if (nextRoadPlane != null && player != null && player.transform.position.z >= nextRoadPlane.transform.position.z-roadPlaneLength/2 )
         {
-            Vector3 nextRoadPlanePosition = new Vector3(0, 0, roadPlaneZ + roadPlaneLength);
-            Instantiate(roadPlanePrefab, nextRoadPlanePosition, Quaternion.identity);
-            isRoadPlaneSpawned = true;
+            Destroy(currentRoadPlane);
+            currentRoadPlane = nextRoadPlane;
+            nextRoadPlane = Instantiate(roadPlanePrefab, new Vector3(0, 0, currentRoadPlane.transform.position.z + roadPlaneLength), Quaternion.identity);
+            // isRoadPlaneSpawned = true;
         }
     }
 }
